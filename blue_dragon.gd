37c158e0a957eed4cee8047ca72af65ca592a9dd
Bence Kovács <67001726+Kok0124@users.noreply.github.com>
2024-05-27 17:34:01 +0200

    Dino movement

--- blue_dragon.gd
+++ blue_dragon.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 500.0
+const JUMP_VELOCITY = -600.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var GRAVITY = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,15 +14,23 @@ func _physics_process(delta):
 		if not get_parent().game_running:
 			$AnimatedSprite2D.play("default")
 		else:
-			$RunCol.disabled = false
+			#$RunCol.disabled = false
 			if Input.is_action_pressed("ui_accept"):
 				velocity.y = JUMP_VELOCITY
 			elif Input.is_action_pressed("ui_down"):
 				$AnimatedSprite2D.play("duck")
 				$RunCol.disabled = true
+			elif Input.is_action_pressed("move_right"):
+				$AnimatedSprite2D.play("spring")
+				velocity.x += 1
+			elif Input.is_action_pressed("move_left"):
+				$AnimatedSprite2D.play("default")
+				velocity.x -= 1
 			else:
-				$AnimatedSprite2D.play("run")
+				$AnimatedSprite2D.play("running")
+				velocity.x = 0
 	else:
 		$AnimatedSprite2D.play("jump")
-		
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, get_viewport_rect().size)
 	move_and_slide()
